d072985ac1ecb3cd758c8693a02ab6558784dcab
raphael groemmer <r.groemmer@yahoo.de>
2020-11-19 17:30:34 +0100

    added `HttpProxy` and `HttpNoProxy` attribute to BackupSpec in etcd_types.go

--- api/v1alpha1/etcd_types.go
+++ api/v1alpha1/etcd_types.go
@@ -97,6 +97,12 @@ type BackupSpec struct {
 	// DeltaSnapshotMemoryLimit defines the memory limit after which delta snapshots will be taken
 	// +optional
 	DeltaSnapshotMemoryLimit *resource.Quantity `json:"deltaSnapshotMemoryLimit,omitempty"`
+	// HttpProxy defines the environment variable for the proxy endpoint
+	// +optional
+	HttpProxy *string `json:"httpProxy,omitempty"`
+	// HttpNoProxy defines the environment variable for the no_proxy setting
+	// +optional
+	HttpNoProxy *string `json:"httpNoProxy,omitempty"`
 }
 
 // EtcdConfig defines parametes associated etcd deployed

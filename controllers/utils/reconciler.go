da3ed3c7d4c78b0223596ad1b26f579026a168dd
Marcel Boehm <marcel.boehm@inovex.de>
2024-06-10 12:29:27 +0200

    Remove file dependencies on local chart folder
    This commit can probably be dropped after https://github.com/gardener/etcd-druid/pull/777
    (probably v0.23.x)

--- controllers/utils/reconciler.go
+++ controllers/utils/reconciler.go
@@ -15,29 +15,15 @@
 package utils
 
 import (
-	"path/filepath"
-
-	"github.com/gardener/etcd-druid/pkg/common"
+	"github.com/gardener/etcd-druid/charts"
 	"github.com/gardener/gardener/pkg/utils/imagevector"
 )
 
-const (
-	// defaultImageVector is a constant for the path to the default image vector file.
-	defaultImageVector = "images.yaml"
-)
-
-// getImageYAMLPath returns the path to the image vector YAML file.
-// The path to the default image vector YAML path is returned, unless `useEtcdWrapperImageVector`
-// is set to true, in which case the path to the etcd wrapper image vector YAML is returned.
-func getImageYAMLPath() string {
-	return filepath.Join(common.ChartPath, defaultImageVector)
-}
-
-// CreateImageVector creates an image vector from the default images.yaml file or the images-wrapper.yaml file.
+// CreateImageVector creates an image vector from the default images.yaml file.
 func CreateImageVector() (imagevector.ImageVector, error) {
-	imageVector, err := imagevector.ReadGlobalImageVectorWithEnvOverride(getImageYAMLPath())
+	imgVec, err := imagevector.Read(charts.ImagesYAML)
 	if err != nil {
 		return nil, err
 	}
-	return imageVector, nil
+	return imagevector.WithEnvOverride(imgVec)
 }
